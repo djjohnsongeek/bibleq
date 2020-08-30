6ff8ea8234132985eb178ba2d418c2695b6514cb
Daniel <danieleejohnson@gmail.com>
2020-08-29 22:30:43 -0400

    added dummy tests

---
+++ tests/test_auth.py
@@ -0,0 +1,12 @@
+import unittest
+
+class TestAuthRoute(unittest.TestCase):
+
+    def setUp(self):
+        pass
+
+    def tearDown(self):
+        pass
+
+    def test_test_test(self):
+        self.assertFalse(False)
---
+++ tests/test_database.py
@@ -0,0 +1,11 @@
+import unittest
+
+class DataBaseTests(unittest.TestCase):
+    def setUp(self):
+        pass
+
+    def tearDown(self):
+        pass
+
+    def test_test(self):
+        self.assertTrue(True)
