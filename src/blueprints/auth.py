ca71c62d2547e7945e6d68f32af646bf2efb7630
Daniel <danieleejohnson@gmail.com>
2020-08-29 22:00:47 -0400

    started auth routes

---
+++ src/blueprints/auth.py
@@ -0,0 +1,31 @@
+from flask import (
+    Blueprint,
+    flash,
+    redirect,
+    render_template,
+    request,
+    session,
+    url_for,
+    g,
+)
+
+import werkzeug.security as security
+from bibleq.src.classes.DataFetcher import DataFetcher
+
+blue_print = Blueprint('auth', __name__, url_prefix='/auth')
+
+@blue_print.route('/register', methods = ('GET', 'POST'))
+def register():
+    data_fetcher = DataFetcher(g.db)
+    answered_qs = data_fetcher.get_answered_questions()
+    unanswered_qs = data_fetcher.get_unanswered_questions()
+    return "register"
+
+
+@blue_print.route('/login', methods = ('GET', 'POST'))
+def login():
+   return "login"
+
+@blue_print.route('/logout')
+def logout():
+    return "logout"
