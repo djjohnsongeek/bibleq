16b348229d05e376b9b05313444726fdfa8b8c4f
Daniel <danieleejohnson@gmail.com>
2020-08-30 22:43:17 -0400

    removed testing code, left todo functions

--- blueprints/auth.py
+++ blueprints/auth.py
@@ -10,24 +10,18 @@ from flask import (
 )
 
 # user current_app insite a request to access app
-
 import werkzeug.security as security
-from classes.DataFetcher import DataFetcher
 
 blue_print = Blueprint('auth', __name__, url_prefix='/auth')
 
 @blue_print.route('/register', methods = ('GET', 'POST'))
 def register():
-    data_fetcher = DataFetcher(g.db)
-    answered_qs = data_fetcher.get_answered_questions()
-    unanswered_qs = data_fetcher.get_unanswered_questions()
     return "register"
-
 
 @blue_print.route('/login', methods = ('GET', 'POST'))
 def login():
    return "login"
 
-@blue_print.route('/logout')
+@blue_print.route('/logout', methods = ('GET'))
 def logout():
     return "logout"
