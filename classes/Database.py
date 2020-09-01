c8b5a532b2d2d4083c8747c1873bbfc66c834589
Daniel <danieleejohnson@gmail.com>
2020-08-31 23:16:24 -0400

    added set_db(), execute_sql_file(), parse_sql_file(), improved connect() and close()

--- classes/Database.py
+++ classes/Database.py
@@ -1,21 +1,36 @@
 import pymysql
 
+class DatabaseTransitionError(Exception):
+    """ Raised when current db is switched while a connection is active """
+    def __init__(self):
+        self.message = "Database cannot be switched while a connection is open."
+
 class Database:
-    conn = None
+
+    def __init__(self):
+        self.conn = None
+        self.testing = False
+        self.databases = {
+            'dev': 'bibleq',
+            'test': 'bibleq_test'
+        }
+        self.current_db = None
 
     def connect(self):
-        self.conn = pymysql.connect(
-            host = self.db_params['host'],
-            user = self.db_params['user'],
-            password = self.db_params['password'],
-            port = self.db_params['port'],
-            db = self.db_params['db'],
-            charset = self.db_params['charset'],
-            cursorclass = self.db_params['cursorclass'],
-        )
+        if self.conn is None:
+            self.conn = pymysql.connect(
+                host = self.db_params['host'],
+                user = self.db_params['user'],
+                password = self.db_params['password'],
+                port = self.db_params['port'],
+                db = self.db_params['db'],
+                charset = self.db_params['charset'],
+                cursorclass = self.db_params['cursorclass'],
+            )
 
     def close(self):
-        self.conn.close()
+        if self.conn is not None:
+            self.conn.close()
         self.conn = None
 
     def init(self, app_config):
@@ -28,9 +43,73 @@ class Database:
             charset = app_config['MYSQL_CHARSET'],
             cursorclass = pymysql.cursors.DictCursor,
         )
-    
-    # parse sql
-    # init db
+        self.schema_path = app_config['SCHEMA_SQL_PATH']
+
+        if app_config['TESTING']:
+            self.testing = True
+            self.set_db('test')
+        else:
+            self.testing = False
+            self.set_db('dev')
+
+    def set_db(self, db_name):
+        if self.conn is None:
+            self.current_db = self.databases[db_name]
+        else:
+            raise DatabaseTransitionError
+
+
+    def execute_sql_file(self, file_path):
+        statements = self.parse_sql_file(file_path)
+        self.connect()
+
+        cur = self.conn.cursor()
+
+        try:
+            for statement in statements:
+                cur.execute(statement)
+        except:
+            result = False
+        else:
+            result = True
+        finally:
+            if result: self.conn.commit()
+            cur.close()
+
+        return result
+
+    def parse_sql_file(self, file_path):
+        DELIMITER = ';'
+        NEW_LINE = '\n'
+        COMMENT_PREFIX = ('--', '/*')
+
+        statements = []
+
+        with open(file_path) as f:
+            statement = ''
+            for line in f:
+                # skip empty or comment lines
+                if line == NEW_LINE or line.startswith(COMMENT_PREFIX):
+                    continue
+
+                statement = statement + line.rstrip(NEW_LINE)
+
+                if statement.endswith(DELIMITER):
+                    statements.append(statement)
+                    statement = ''
+
+        return statements
+
+    def init_test_db(self):
+        self.connect()
+
+        cur = self.conn.cursor()
+        cur.execute('CREATE DATABASE bibleq_test;')
+
+        self.conn.commit()
+        self.conn.close()
+        self.db_params['db'] = 'bibleq_test'
+        self.execute_sql_file(self.schema_path)
     # populate db
 
 db = Database()
