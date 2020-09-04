ba385de30ab67d726f460ae9357af029d7eb3105
Daniel <danieleejohnson@gmail.com>
2020-09-03 22:34:21 -0400

    added question blueprint

---
+++ blueprints/question.py
@@ -0,0 +1,19 @@
+from flask import (
+    Blueprint,
+    flash,
+    redirect,
+    render_template,
+    request,
+    session,
+    url_for,
+    g,
+)
+
+from classes.Question import Question
+
+blue_print = Blueprint('question', __name__, url_prefix='/question')
+
+
+@blue_print.route('/create', methods=('GET',))
+def create():
+    return render_template('questions/create.html')
