6b61b0e25b275986348f1ab80a10df570eab1bac
Daniel <danieleejohnson@gmail.com>
2020-09-06 14:03:41 -0400

    simple user registration

--- blueprints/auth.py
+++ blueprints/auth.py
@@ -1,3 +1,4 @@
+from pprint import pprint
 from flask import (
     Blueprint,
     flash,
@@ -6,11 +7,13 @@ from flask import (
     request,
     session,
     url_for,
+    current_app,
     g,
 )
 
-# user current_app insite a request to access app
 import werkzeug.security as security
+
+from classes.User import User
 
 blue_print = Blueprint('auth', __name__, url_prefix='/auth')
 
@@ -18,6 +21,20 @@ blue_print = Blueprint('auth', __name__, url_prefix='/auth')
 @blue_print.route('/register', methods=('GET', 'POST'))
 def register():
     if request.method == 'POST':
+        user_data = {
+            'first_name' : request.form.get('first_name'),
+            'last_name' : request.form.get('last_name'),
+            'email': request.form.get('email'),
+            'password': request.form.get('password'),
+            'confirm_pw': request.form.get('confirm_pw'),
+            'account_level': current_app.config['USER_ACCNT'],
+            'question_count': 0,
+            'answer_count': 0,
+        }
+
+        user = User(g.db, user_data)
+        user.create()
+
         flash('Account created', 'info')
         return redirect(url_for('auth.login'))
 
