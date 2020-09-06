7085bd4b3b56f0d8be5fe91ec351c27298858280
Daniel <danieleejohnson@gmail.com>
2020-09-06 14:03:55 -0400

    reformat

--- classes/Question.py
+++ classes/Question.py
@@ -1,22 +1,20 @@
 class Question():
 
     def __init__(self, db, data):
-        self.conn = db.conn
+        self.db = db
 
-        self.poster_id = data["poster_id"]
-        self.writer_id = data["writer_id"]
-        self.answer_id = data.get("answer_id", None)
-        self.body = data["body"]
-        self.unfit_flag_count = data.get("unfit_flag_count", 0)
+        self.poster_id = data['poster_id']
+        self.writer_id = data['writer_id']
+        self.answer_id = data.get('answer_id', None)
+        self.body = data['body']
+        self.unfit_flag_count = data.get('unfit_flag_count', 0)
 
     def create(self):
-        cur = self.conn.cursor()
-        sql = """
-                INSERT INTO questions (
-                    original_poster_id, writer_id,
-                    answer_id, body, unfit_flag_count)
-                VALUES (%s, %s, %s, %s, %s)
-              """
+        cur = self.db.conn.cursor()
+        sql = ('INSERT INTO questions ('
+                    'original_poster_id, writer_id,'
+                    'answer_id, body, unfit_flag_count)'
+              'VALUES (%s, %s, %s, %s, %s)')
 
         cur.execute(
             sql,
@@ -29,6 +27,6 @@ class Question():
             )
         )
 
-        self.conn.commit()
+        self.db.conn.commit()
 
         cur.close()
