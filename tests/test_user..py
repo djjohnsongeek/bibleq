12e322ef62e6c036bcb1856b323ba54cc4302c0b
Daniel <danieleejohnson@gmail.com>
2020-09-06 23:50:32 -0400

    barely started writing tests for User

---
+++ tests/test_user..py
@@ -0,0 +1,35 @@
+import unittest
+
+from bibleq import create_app
+from classes.Database import db
+from classes.User import User
+
+
+class TestUserClass(unittest.TestCase):
+    @classmethod
+    def setUpClass(cls):
+        cls.db = db
+        cls.app = create_app({
+            'TESTING': True,
+            'MYSQL_DB': 'bibleq_test',
+            'TEST_SQL_PATH': r'C:\Users\Johnson\Projects\flask-'
+                             r'app\bibleq\tests\temp\test.sql',
+        })
+
+        cls.db.init(cls.app.config)
+        cls.db.execute_sql_file(cls.db.schema_path)
+
+
+    def setUp(self):
+        pass
+
+    def tearDown(self):
+        pass
+
+
+# test user init
+# test user create
+# test user update
+# test user delete
+# test user get_all
+
