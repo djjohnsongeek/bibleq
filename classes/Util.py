dbdd8d1c5663299a6339104e7d121d86b1694a34
djjohnsongeek <danieleejohnson@gmail.com>
2020-09-21 05:19:47 -0400

    added testing TODOs, add authenticate() method

--- classes/Util.py
+++ classes/Util.py
@@ -1,6 +1,8 @@
+from flask import redirect, url_for, flash
+
 class Util():
 
-    @staticmethod
+    @staticmethod # TODO add tests
     def contains_upper(string: str) -> bool:
 
         if type(string) != str:
@@ -12,7 +14,7 @@ class Util():
 
         return False
 
-    @staticmethod
+    @staticmethod # TODO add tests
     def contains_lower(string: str) -> bool:
 
         for char in string:
@@ -21,7 +23,7 @@ class Util():
 
         return False
 
-    @staticmethod
+    @staticmethod # TODO add tests
     def contains_num(string: str) -> bool:
 
         if type(string) != str:
@@ -32,3 +34,17 @@ class Util():
                 return True
 
         return False
+
+
+    @staticmethod # TODO add tests
+    def authenticate(session, level):
+        user = session.get('user', None)
+
+        if user is None:
+            auth = False
+        elif user['account_level'] < level:
+            auth = False
+        else:
+            auth = True
+
+        return auth
