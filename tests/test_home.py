5f5e50adf98cd9e2b866bbdb88e6f9c82b5519fb
djjohnsongeek <danieleejohnson@gmail.com>
2020-09-21 05:57:45 -0400

    add tests for home.index()

---
+++ tests/test_home.py
@@ -0,0 +1,28 @@
+import unittest
+
+from bibleq import create_app
+from classes.Database import db
+
+
+class TestHomeRoutes(unittest.TestCase):
+
+    @classmethod
+    def setUpClass(cls):
+        # setup app and test client
+        cls.db = db
+        cls.app = create_app({
+            'TESTING': True,
+            'MYSQL_DB': 'bibleq_test',
+            'TEST_SQL_PATH': r'C:\Users\Johnson\Projects\flask-'
+                             r'app\bibleq\tests\temp\test.sql',
+        })
+        cls.client = cls.app.test_client()
+
+        # setup database
+        cls.db.init(cls.app.config)
+        cls.db.execute_sql_file(cls.db.schema_path)
+        
+
+    def test_index(self):
+        response = self.client.get('/')
+        self.assertIn(b'<h1>Bible Q</h1>', response.data)
