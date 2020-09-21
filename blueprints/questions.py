06b49c762abd04318c0f7b731f69d6559812780e
djjohnsongeek <danieleejohnson@gmail.com>
2020-09-21 05:21:00 -0400

    require User login in create() route

--- blueprints/questions.py
+++ blueprints/questions.py
@@ -6,17 +6,22 @@ from flask import (
     request,
     session,
     url_for,
-    g,
+    g, current_app
 )
 
 from classes.Question import Question
+from classes.Util import Util
 
 blue_print = Blueprint('questions', __name__, url_prefix='/questions')
 
 
 @blue_print.route('/create', methods=('GET', 'POST'))
 def create():
-    if request.method == "POST":
+    if not Util.authenticate(session, current_app.config['USER_ACCNT']):
+        flash('Your must login to ask a new question.', 'info')
+        return redirect(url_for('home.index'))
+
+    if request.method == 'POST':
         flash('Your Question has been created.', 'info')
 
     return render_template('questions/create_question.html')
