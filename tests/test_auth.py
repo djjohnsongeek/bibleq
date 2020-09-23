68d12d73c321408f3a307aac122c31fbf62c14f2
Daniel <danieleejohnson@gmail.com>
2020-09-22 21:37:18 -0400

    added tests for login and logout

--- tests/test_auth.py
+++ tests/test_auth.py
@@ -1,11 +1,14 @@
 import unittest
 
+from flask import session
+
 from bibleq import create_app
 from classes.Database import db
 from classes.User import User
 
 # TODO check url with response.headers['Location']
 class TestAuthRoute(unittest.TestCase):
+
     @classmethod
     def setUpClass(cls):
         # setup app and test client
@@ -25,6 +28,41 @@ class TestAuthRoute(unittest.TestCase):
         # set domain name
         cls.domain_name = cls.app.config['APP_URL']
 
+        # create a test user
+        with cls.app.app_context():
+            User(cls.db, {
+                'first_name': 'first',
+                'last_name': 'last',
+                'email': 'test@gmail.com',
+                'password': 'TestPassword123',
+                'confirm_pw': 'TestPassword123',
+                'account_level': cls.app.config['USER_ACCNT'],
+                'question_count': 0,
+                'answer_count': 0,
+            })
+
+    # helper methods
+    def login(self, email, password):
+        return self.client.post(
+            "/auth/login", data = {
+                "email": email,
+                "password": password
+            }
+        )
+
+    def register_test_user(self):
+        return self.client.post(
+            '/auth/register',
+            data = {
+                'first_name': 'first2',
+                'last_name': 'last2',
+                'email': 'test2@gmail.com',
+                'password': 'TestPassword234',
+                'confirm_pw': 'TestPassword234',
+            },
+            follow_redirects=True
+        )
+
     def setUp(self):
         self.db.connect()
 
@@ -33,51 +71,36 @@ class TestAuthRoute(unittest.TestCase):
 
     def test_register_get(self):
         response = self.client.get('/auth/register')
-
         confirm_pw_input = (b'<input type="password" name="confirm_pw" '
                             b'placeholder="Confirm Password"><br/>')
         page_header = b'<h1>Create Account</h1'
-        # page_url = self.domain_name + response.headers.get('Location')
 
         self.assertEqual(200, response.status_code)
-        # self.assertEqual(page_url, self.domain_name + '/auth/register')
         self.assertIn(confirm_pw_input, response.data)
         self.assertIn(page_header, response.data)
 
     def test_register_post(self):
         # successfull user creation
-        response = self.client.post(
-            '/auth/register',
-            data = {
-                'first_name': 'Daniel',
-                'last_name': 'Johnson',
-                'email': 'danieleejohnson@gmail.com',
-                'password': 'DJohnson1234',
-                'confirm_pw': 'DJohnson1234',
-            },
-            follow_redirects=True
-        )
-        # page_url = self.domain_name + response.headers.get('Location')
+        response = self.register_test_user()
 
         self.assertEqual(200, response.status_code)
-        # self.assertEqual(page_url, self.domain_name + '/auth/login')
         self.assertIn(b'<h1>Login</h1>', response.data)
         self.assertIn(b'Account created. Please login.', response.data)
 
-        user_info = User.get_user_info(self.db, 1, None)
+        user_info = User.get_user_info(self.db, None, 'test2@gmail.com')
 
-        self.assertEqual('Daniel', user_info['first_name'])
-        self.assertEqual('Johnson', user_info['last_name'])
+        self.assertEqual('first2', user_info['first_name'])
+        self.assertEqual('last2', user_info['last_name'])
 
         # failed user creation
         response = self.client.post(
             '/auth/register',
             data = {
-                'first_name': 'Daniel',
-                'last_name': 'Johnson',
-                'email': 'danieleejohnson@gmail.com',
-                'password': 'DJOhnson1234',
-                'confirm_pw': 'DJohnson1234',
+                'first_name': 'first',
+                'last_name': 'last',
+                'email': 'test2@gmail.com',
+                'password': 'TestPassword234',
+                'confirm_pw': 'TestPassword123',
             },
             follow_redirects = True
         )
@@ -88,6 +111,36 @@ class TestAuthRoute(unittest.TestCase):
         self.assertIn(b'Passwords do not match!', response.data)
         self.assertIn(b'User with this email already exists.', response.data)
 
+        # remove all users
+        self.db.trucate_table('users')
+
+    def test_login(self):
+        # GET Request
+        response = self.client.get('/auth/login')
+        self.assertEqual(200, response.status_code)
+        self.assertIn(
+            b'<button type="submit">Login</button>',
+            response.data
+        )
+
+        # POST Request
+
+        # no one logged in
+        with self.client:
+            self.client.get('/')
+            self.assertIsNone(session.get('user', None))
+
+        # login
+        response = self.login('test@gmail.com', 'TestPassword123')
+        self.assertEqual(302, response.status_code)
+
+        with self.client:
+            self.client.get('/')
+            self.assertTrue(session.get('user', None))
+
+        # logout, clean out registered users
+        self.client.get('/auth/logout')
+
     def test_logout(self):
         # test logout link when no one is logged in
         response = self.client.get('/auth/logout', follow_redirects=True)
@@ -96,10 +149,13 @@ class TestAuthRoute(unittest.TestCase):
         self.assertIn(b'<h1>Bible Q</h1', response.data)
         self.assertNotIn(b'You have been logged out!', response.data)
 
-        # TODO test logout when someone is logged in
-        # login()
-        # response = self.client.get('/auth/logout', follow_redirects=True)
+        # logout when someone is logged in
+        self.login('test@gmail.com', 'TestPassword123')
+        response = self.client.get('/auth/logout', follow_redirects=True)
 
-        # self.assertEqual(200, response.status_code)
-        # self.assertIn(b'<h1>Bible Q</h1', response.data)
-        # self.assertIn(b'You have been logged out!', response.data)
+        self.assertEqual(200, response.status_code)
+        self.assertIn(b'<h1>Bible Q</h1', response.data)
+        self.assertIn(b'You have been logged out!', response.data)
+        with self.client:
+            self.client.get('/')
+            self.assertIsNone(session.get('user', None))
