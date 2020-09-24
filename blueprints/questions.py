8750ae3a6534c4205738e706642052c66baa97a2
Daniel <danieleejohnson@gmail.com>
2020-09-23 22:55:00 -0400

    itial pass on question creation route

--- blueprints/questions.py
+++ blueprints/questions.py
@@ -6,10 +6,12 @@ from flask import (
     request,
     session,
     url_for,
-    current_app
+    current_app,
+    g,
 )
 
 from classes.Util import Util
+from classes.Question import Question
 
 blue_print = Blueprint('questions', __name__, url_prefix='/questions')
 
@@ -17,10 +19,25 @@ blue_print = Blueprint('questions', __name__, url_prefix='/questions')
 @blue_print.route('/create', methods=('GET', 'POST'))
 def create():
     if not Util.authenticate(session, current_app.config['USER_ACCNT']):
-        flash('Your must login to ask a new question.', 'info')
+        flash('You must login to ask a new question.', 'info')
         return redirect(url_for('home.index'))
 
     if request.method == 'POST':
-        flash('Your Question has been created.', 'info')
+
+        q_data = {
+            'title': request.form.get('question_title'),
+            'body': request.form.get('question_body'),
+            'poster_id': session['user']['user_id'],
+            'writer_id': None,
+            'answer_id': None,
+            'unfit_flag_cont': 0,
+        }
+        new_question = Question(g.db, q_data)
+
+        if not new_question.errors:
+            flash('Your Question has been created.', 'info')
+        else:
+            for error in new_question.errors:
+                flash(error, 'error')
 
     return render_template('questions/create_question.html')
